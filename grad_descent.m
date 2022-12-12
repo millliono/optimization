ffd0d8f362cca442e5f4c0efe6147a59265a21c6
millliono <57071317+millliono@users.noreply.github.com>
2022-12-12 17:21:59 +0200

    Update grad_descent.m

--- grad_descent.m
+++ grad_descent.m
@@ -1,5 +1,5 @@
-function [xmin,ymin] = grad_descent(xinit,yinit)
-%f = @(x,y) x^5 * exp(-x^2 - y^2);
+function [xmin,ymin] = grad_descent(xinit,yinit,method)
+f = @(x,y) x^5 * exp(-x^2 - y^2);
 
 dfdx = @(x,y) 5*x^4*exp(- x^2 - y^2) - 2*x^6*exp(- x^2 - y^2);
 
@@ -7,19 +7,33 @@ dfdy = @(x,y) -2*x^5*y*exp(- x^2 - y^2);
 x = xinit;
 y = yinit;
 
-% Set the learning rate
-alpha = 0.0001;
-
 % Set stop condition threshhold
 e = 0.1e-4;
 
-% Perform gradient descent
-n_grad = norm([dfdx(x,y), dfdy(x,y)]);
-while n_grad > e
-    grad = [dfdx(x,y), dfdy(x,y)];
-    n_grad = norm(grad);
-    x = x - alpha * grad(1);
-    y = y - alpha * grad(2);
+if strcmp(method, "const")
+    alpha = 0.0001;
+    n_grad = norm([dfdx(x,y), dfdy(x,y)]);
+    while n_grad > e
+        grad = [dfdx(x,y), dfdy(x,y)];
+        n_grad = norm(grad);
+        x = x - alpha * grad(1);
+        y = y - alpha * grad(2);
+    end
+
+elseif strcmp(method, "line_min")
+    n_grad = norm([dfdx(x,y), dfdy(x,y)]);
+    while n_grad > e
+        grad = [dfdx(x,y), dfdy(x,y)];
+        n_grad = norm(grad);
+        alpha = linspace(0,10,1000);
+        map = arrayfun(f, x - alpha * grad(1), y - alpha * grad(2));
+        [~, idx] = min(map); 
+        x = x - alpha(idx) * grad(1);
+        y = y - alpha(idx) * grad(2);
+    end
+
+elseif strcmp(method, "armijo")
+ 
 end
 
 xmin = x;
